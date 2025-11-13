372481f601b6f6cbb4db7d1dd9715e0d85a16b22
mohammedari <mohammedarixx@gmail.com>
2025-11-13 21:38:21 +0900

    easy_002 done in training.

--- atcoder_problems/training/easy_002.cpp
+++ atcoder_problems/training/easy_002.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <vector>
+#include <limits>
+#include <numeric>
 
 int main() {
-  std::cout << "Hello World!" << std::endl;
+  int N;
+  std::cin >> N;
+
+  std::vector<int> X(N);
+  for (auto i = 0; i < N; ++i) {
+    std::cin >> X[i];
+  }
+
+  auto min_sum = std::numeric_limits<int>::max();
+  for (auto P = 1; P <= 100; ++P) {
+
+    auto sum = std::accumulate(X.begin(), X.end(), 0, [P](int acc, int x) {
+      return acc + (x - P) * (x - P);
+    });
+
+    if (sum < min_sum)
+      min_sum = sum;
+  }
+
+  std::cout << min_sum << std::endl;
   return 0;
 }
